bb21b21b409b6441d58cbfc418e8f1b6151a58c9
Allan <allan.landin25@gmail.com>
2024-03-10 11:52:02 -0300

    Add the environment variable  in connection with database

--- infra/database.js
+++ infra/database.js
@@ -5,6 +5,7 @@ const pool = mysql.createPool({
   user: process.env.MYSQL_USER,
   database: process.env.MYSQL_DATABASE,
   password: process.env.MYSQL_PASSWORD,
+  port: process.env.MYSQL_PORT,
 });
 
 async function query(queryObj) {
